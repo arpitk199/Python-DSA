9a3f9b642c8c3fa69b6bfd1f0f4182b780376991
arpitk199 <kumararpi199@gmail.com>
2024-04-01 12:39:55 +0530

    Commit

---
+++ 1.Basics/1_print_statement.py
@@ -0,0 +1,3 @@
+print("Hello World", end=" ")
+print("Good Bye",end=" ")
+print("We are Learning Python")
---
+++ 1.Basics/3_comments.py
@@ -0,0 +1,11 @@
+print("Hello world") #this is a print statement
+
+"""
+DocSTRING
+multi Line comment
+djkbcihsdckbsdkjcsd
+sdkbcsdbcbdicbidc
+csdlnciosbciosdocsdo
+csdcnisdcisdoc
+klsdncjsndcjosdo
+"""
